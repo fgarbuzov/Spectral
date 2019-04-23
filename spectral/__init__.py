73b4001a6d61c5e1204d16872aca6cb43e3a3d4f
theodor_garbuzov <fedor.garbuzov@gmail.com>
2019-04-23 15:54:30 +0300

    Added Constant class and comparison with solitons in cylinder (2 attempt)

--- spectral/__init__.py
+++ spectral/__init__.py
@@ -3,10 +3,11 @@ from .progress import *
 from . import legendre
 from .legendre import *
 from .fourier import *
+from .constant import *
 from .wave import *
 from .multidomain import *
 from .mesh import *
 from .tensorfield import *
 from .plot import *
 
-__version__ = '1.03.1'
+__version__ = '1.02.5'
--- spectral/constant.py
+++ spectral/constant.py
@@ -2,20 +2,15 @@ import numpy as np
 import scipy as sp
 from .mesh import Mesh1D
 
-class Wave(Mesh1D):
-    def __init__(self, k):
+class Constant(Mesh1D):
+    def __init__(self):
         self.shape = 1,
-        self.k = k
     
     def grid(self):
         return np.array([0.]),
         
     def __eq__(self, mesh):
-        if id(self) == id(mesh):
-            return True
-        if not isinstance(mesh, Wave):
-            return False
-        return mesh.k == self.k
+        return isinstance(mesh, Constant)
         
     def __ne__(self, mesh):
         return not (self == mesh)    
@@ -23,8 +18,8 @@ class Wave(Mesh1D):
     def diff(self, func, axis, dim, bval):
         "Calculate a derivative along a given axis"
         if dim != 0:
-            raise ValueError("Wave is one-dimensional")
-        return 1j*self.k*func
+            raise ValueError("Constant is one-dimensional")
+        return np.zeros_like(func)
         
     def match_domains(self, func, axes):
         "Match mesh domains"
@@ -45,5 +40,4 @@ class Wave(Mesh1D):
         if x is None:
             return func
         func = np.squeeze(func, axis)
-        A = np.exp(1j*self.k*np.asarray(x))
-        return np.multiply.outer(A, func)
+        return np.resize(func, np.shape(x) + func.shape)
